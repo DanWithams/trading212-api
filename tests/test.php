e8db5def7e5bffb8ccc708b6b37dbce5f23837b1
Daniel Withams <me@danwithams.com>
2024-06-14 23:14:06 +0100

    Removing key, which has already been revoke for any bad actors who may be thinking nefarious things

--- tests/test.php
+++ tests/test.php
@@ -3,8 +3,8 @@
 include __DIR__ . '/../vendor/autoload.php';
 
 $client = new \DanWithams\Trading212Api\ClientConfig(
-    hostname: 'live.trading212.com',
-    secret: '22077901ZHOhCAlNsOjIFSTaeEcXmXydAvUuk'
+    hostname: 'demo.trading212.com',
+    secret: '--'
 );
 
 $trading212 = new \DanWithams\Trading212Api\Trading212($client);
